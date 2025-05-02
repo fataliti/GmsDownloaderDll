d7bdbb11d50b974aca7dab8814d0d7dae1234210
fataliti22@gmail.com <Ivan Reifshneider>
2025-05-02 16:54:08 +0300

    Добавьте файлы проекта.

---
+++ GmsDownloaderDll/Download.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+
+
+namespace GmsDownloaderDll
+{
+    internal class Download
+    {
+        private readonly WebClient _wc = new WebClient();
+
+        private static double _id;
+
+        public int Progress;
+
+        public readonly double Id;
+
+        public readonly string Name;
+
+        public double Complete;
+
+        public Download(string link, string fileName)
+        {
+            Id = _id++;
+            Name = fileName;
+            _wc.DownloadProgressChanged += ProgressChanged;
+            _wc.DownloadFileCompleted += Completed;
+            _wc.DownloadFileAsync(new Uri(link), fileName);
+            Console.WriteLine("Download start with Id: " + Id);
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Progress = e.ProgressPercentage;
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                Complete = -1.0;
+                Console.WriteLine("Download failed: " + Id);
+            }
+            else
+            {
+                Complete = 1.0;
+                Console.WriteLine("Download completed: " + Id);
+            }
+        }
+
+        public void CancelDownload()
+        {
+            _wc.CancelAsync();
+        }
+    }
+}
---
+++ GmsDownloaderDll/DownloadManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GmsDownloaderDll
+{
+    public class DownloadManager
+    {
+        private static Dictionary<double, Download> _downloads = new Dictionary<double, Download>();
+
+        private static string _downDir = "";
+
+        [DllExport("DownloadDirectory")]
+        public static string DownloadDirectory(string dir)
+        {
+            _downDir = dir;
+            return dir;
+        }
+
+
+        [DllExport("DownloadFile")]
+        public static double DownloadFile(string link, string fileName)
+        {
+            Download download = new Download(link, _downDir + fileName);
+            _downloads.Add(download.Id, download);
+            return download.Id;
+        }
+
+        [DllExport("DownloadDelete")]
+        public static double DownloadDelete(double downloadId)
+        {
+            if (!_downloads.ContainsKey(downloadId))
+            {
+                return -1.0;
+            }
+            _downloads[downloadId].CancelDownload();
+            _downloads.Remove(downloadId);
+            return 1.0;
+        }
+
+        [DllExport("DownloadStatus")]
+        public static double DownloadStatus(double downloadId)
+        {
+            if (!_downloads.ContainsKey(downloadId))
+            {
+                return -1.0;
+            }
+            return Convert.ToDouble(_downloads[downloadId].Progress);
+        }
+
+        [DllExport("DownloadComplete")]
+        public static double DownloadComplete(double downloadId)
+        {
+            if (!_downloads.ContainsKey(downloadId))
+            {
+                return -2.0;
+            }
+            return _downloads[downloadId].Complete;
+        }
+
+        [DllExport("DownloadResult")]
+        public static string DownloadResult(double downloadId)
+        {
+            if (!_downloads.ContainsKey(downloadId))
+            {
+                return "";
+            }
+            return (_downDir == "") ? (Directory.GetCurrentDirectory() + "\\" + _downloads[downloadId].Name) : _downloads[downloadId].Name;
+        }
+    }
+}
